b81355a48f293e3a620665b4cc000d7d95c7352c
Mister Singh <mistersingh179@gmail.com>
2023-06-04 16:47:30 -0400

    updated test bg job to run every hour instead of every minute
    weeklyProductRecommendations is now every hour

--- defer/weeklyProductRecommendations.ts
+++ defer/weeklyProductRecommendations.ts
@@ -1,7 +1,8 @@
 import { defer } from "@defer/client";
 
 const weeklyProductRecommendations = async () => {
-  console.log("in bg job: started weeklyProductRecommendations");
+  console.log("started bg job: started weeklyProductRecommendations");
+  console.log("finished bg job: started weeklyProductRecommendations");
 };
 
-export default defer.cron(weeklyProductRecommendations, "* * * * *");
+export default defer.cron(weeklyProductRecommendations, "0 * * * *");
