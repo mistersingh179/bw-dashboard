9c8eb9adb2177625787df92e2f7e883b1921c3b3
Mister Singh <mistersingh179@gmail.com>
2023-09-19 14:39:42 -0400

    added txName to api logs

--- middlewares/loggingMiddleware.ts
+++ middlewares/loggingMiddleware.ts
@@ -1,9 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { Middleware } from "next-api-middleware";
-import { nanoid } from "nanoid";
 import logger from "@/lib/logger";
-import {isArray, pick} from "lodash";
-import newrelic from 'newrelic';
+import { isArray, pick } from "lodash";
+import newrelic from "newrelic";
 
 const loggingMiddleware: Middleware = async (req, res, next) => {
   const beforeTime = Date.now();
@@ -11,27 +9,30 @@ const loggingMiddleware: Middleware = async (req, res, next) => {
   const reqItems = pick(req, ["method", "url", "query", "body"]);
   const trueClientIp = req.headers["true-client-ip"];
 
-  logger.info({ ...reqItems, trueClientIp, reqId: req.reqId }, "API Request");
-
-  if(req.url && req.query){
-    let txName = req.url;
-    for(let [key, value] of Object.entries(req.query)){
-      if(isArray(value)){
+  let txName = req.url;
+  if (txName && req.query) {
+    for (let [key, value] of Object.entries(req.query)) {
+      if (isArray(value)) {
         value = value[0];
       }
-      if(value){
+      if (value) {
         txName = txName.replace(value, key);
       }
     }
     newrelic.setTransactionName(txName);
   }
 
+  logger.info(
+    { ...reqItems, trueClientIp, reqId: req.reqId, txName },
+    "API Request"
+  );
+
   await next();
 
   const responseTime = Date.now() - beforeTime;
   const resItems = pick(res, ["statusCode", "statusMessage"]);
   logger.info(
-    { ...reqItems, ...resItems, reqId: req.reqId, responseTime },
+    { ...reqItems, ...resItems, reqId: req.reqId, responseTime, txName },
     "API Response"
   );
 };
