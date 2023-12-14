feecba65545f50865f62a4f671c212a9e4129af6
Mister Singh <mistersingh179@gmail.com>
2023-12-14 14:27:04 -0500

    dropping indexes which we dont need as per pghero

---
+++ prisma/migrations/20231214192629_removing_unnecessary_indexes_as_per_pghero/migration.sql
@@ -0,0 +1,20 @@
+-- DropIndex
+DROP INDEX "Advertisement_advertisementSpotId_idx";
+
+-- DropIndex
+DROP INDEX "Auction_userAgent_idx";
+
+-- DropIndex
+DROP INDEX "Auction_websiteId_createdAt_idx";
+
+-- DropIndex
+DROP INDEX "Campaign_userId_idx";
+
+-- DropIndex
+DROP INDEX "Content_webpageId_idx";
+
+-- DropIndex
+DROP INDEX "Webpage_websiteId_idx";
+
+-- DropIndex
+DROP INDEX "Website_userId_idx";
